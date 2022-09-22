9e703cb844f2ac5ac01314aa210b90e0dae1d314
augustobritodev <augustobritodev@icloud.com>
2022-09-22 16:53:08 -0300

    chore: add new action

---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+const shell = require('shelljs');
+const fs = require('fs')
+
+async function run() {
+  try {
+    const dateTime = (new Date()).toLocaleString('pt-BR');
+
+    const path = core.getInput('path');
+
+    if (!fs.existsSync(path)) { 
+      throw new Error('Path not found');
+    }
+
+    const { 
+      ref,
+      eventName
+    } = github.context;
+
+    const {
+      repository
+    } = github.context.payload
+
+
+    shell.echo(`💡 Job started at ${dateTime}`);
+    shell.echo(`🖥️ Job was automatically triggered by ${eventName} event`);
+    shell.echo(`🔎 The name of your branch is ${ref} and your repository is ${repository.name}.`)
+   
+    shell.echo(process.env);
+
+    shell.echo(`🎉 Job has been finished`);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+} 
+
+
+run();
